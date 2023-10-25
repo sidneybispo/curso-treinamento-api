e80b72f26f448acddcc82b74df7e65368ac92214
sidneybispo <protecney@gmail.com>
2023-10-25 14:29:36 -0300

    classe user

--- src/main/java/br/com/sidneydev/api/ApiApplication.java
+++ src/main/java/br/com/sidneydev/api/ApiApplication.java
@@ -1,5 +1,6 @@
 package br.com.sidneydev.api;
 
+import br.com.sidneydev.api.domain.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 

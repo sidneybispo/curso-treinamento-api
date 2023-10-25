5f247781aa333b44ba8b4913bb557a07bde79f04
sidneybispo <protecney@gmail.com>
2023-10-25 18:14:29 -0300

    classe user resource findById

--- src/main/java/br/com/sidneydev/api/resources/UserResource.java
+++ src/main/java/br/com/sidneydev/api/resources/UserResource.java
@@ -1,6 +1,8 @@
 package br.com.sidneydev.api.resources;
 
 import br.com.sidneydev.api.domain.User;
+import br.com.sidneydev.api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,9 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/user")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id){
 
-        return ResponseEntity.ok().body(new User(1, "sidney", "ney@mail.com", "123"));
+        return ResponseEntity.ok().body(service.findById(id));
     }
 }

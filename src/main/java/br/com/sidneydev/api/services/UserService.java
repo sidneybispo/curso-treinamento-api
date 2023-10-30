c4e2a176d011c9edac839aaa09286e5275b5fd30
sidneybispo <protecney@gmail.com>
2023-10-30 08:36:40 -0300

    endpoint findAll para user

--- src/main/java/br/com/sidneydev/api/services/UserService.java
+++ src/main/java/br/com/sidneydev/api/services/UserService.java
@@ -2,8 +2,11 @@ package br.com.sidneydev.api.services;
 
 import br.com.sidneydev.api.domain.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User findById(Integer id);
+    List<User> findAll();
 
 }

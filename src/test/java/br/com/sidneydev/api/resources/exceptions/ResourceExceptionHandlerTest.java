e9c49650e522b48eef593e1438a0700f7e78b072
sidneybispo <protecney@gmail.com>
2023-11-07 15:34:22 -0300

    teste para manipulador de excessoes metodo data integrity

--- src/test/java/br/com/sidneydev/api/resources/exceptions/ResourceExceptionHandlerTest.java
+++ src/test/java/br/com/sidneydev/api/resources/exceptions/ResourceExceptionHandlerTest.java
@@ -1,5 +1,6 @@
 package br.com.sidneydev.api.resources.exceptions;
 
+import br.com.sidneydev.api.services.exceptions.DataIntegratyViolationException;
 import br.com.sidneydev.api.services.exceptions.ObjectNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,13 +11,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockHttpServletRequest;
 
+import java.time.LocalDateTime;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class ResourceExceptionHandlerTest {
 
     public static final String OBJETO_NAO_ENCONTRADO = "Objeto nao encontrado";
-    //public static final String OBJETO_NAO_ENCONTRADO = "Objeto nao encontrado";
+    public static final String EMAIL_JA_CADASTRADO = "Email ja cadastrado";
+
     @InjectMocks
     private ResourceExceptionHandler exceptionHandler;
 
@@ -42,6 +46,20 @@ class ResourceExceptionHandlerTest {
     }
 
     @Test
-    void dataIntegratyViolationException() {
+    void dataIntegrityViolationException() {
+        ResponseEntity<StandardError> response = exceptionHandler
+                .dataIntegrityViolationException(
+                        new DataIntegratyViolationException(EMAIL_JA_CADASTRADO),
+                        new MockHttpServletRequest());
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(StandardError.class, response.getBody().getClass());
+        assertEquals(EMAIL_JA_CADASTRADO, response.getBody().getError());
+        assertEquals(400, response.getBody().getStatus());
+        assertNotEquals("/user/2", response.getBody().getPath());
+        assertNotEquals(LocalDateTime.now(), response.getBody().getTimestamp());
     }
 }
